b90776f957d7dc39e613a5b5399f3b6fc3a413eb
xunxxoie <ahh0520@naver.com>
2024-11-13 02:13:00 +0900

    [Init] Init Spring

---
+++ AnyoneToo/src/main/java/org/anyonetoo/anyonetoo/AnyoneTooApplication.java
@@ -0,0 +1,13 @@
+package org.anyonetoo.anyonetoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyoneTooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnyoneTooApplication.class, args);
+    }
+
+}
---
+++ AnyoneToo/src/test/java/org/anyonetoo/anyonetoo/AnyoneTooApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anyonetoo.anyonetoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyoneTooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
